961433b8f5ce62fd6912346c85d7e64a01b33d09
Sahil <sahilsharma86600@gmail.com>
2026-03-06 14:18:53 +0530

    root url for admin

--- test/dashboard/components/adminUrl.php
+++ test/dashboard/components/adminUrl.php
@@ -4,7 +4,7 @@
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off'
     || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 
-$root_url = 'http://localhost/book/test/dashboard/';
-// $root_url = $protocol . $_SERVER['HTTP_HOST'] . '/test/dashboard/';
+// $root_url = 'http://localhost/book/test/dashboard/';
+$root_url = $protocol . $_SERVER['HTTP_HOST'] . '/test/dashboard/';
 
 ?>
